fb37feba7b0ae8103586cb5774d89df80b06e819
Ali Biari <abiari@e1r2p2.1337.ma>
2020-12-25 18:41:36 +0100

    added setup scrip for grafana/influxdb and telegraf conf in all images

--- setup.sh
+++ setup.sh
@@ -9,6 +9,9 @@ docker build srcs/wordpress -t wordpress-local
 docker build srcs/mysql -t mysql-local
 docker build srcs/phpmyadmin -t phpmyadmin-local
 docker build srcs/nginx -t nginx-local
+#docker build srcs/grafana -t grafana-local
+#docker build srcs/influxdb -t influxdb-local
+#docker build srcs/ftps -t ftps-local
 
 #MetalLB Setup
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
@@ -21,3 +24,6 @@ kubectl apply -f srcs/yamls/wordpress.yaml
 kubectl apply -f srcs/yamls/phpmyadmin.yaml
 kubectl apply -f srcs/yamls/mysql.yaml
 kubectl apply -f srcs/yamls/nginx.yaml
+#kubectl apply -f srcs/yamls/grafana.yaml
+#kubectl apply -f srcs/yamls/influxdb.yaml
+#kubectl apply -f srcs/yamls/ftps.yaml
