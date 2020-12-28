8f2a556c05f1441f74e7035ae6933370f37a98c4
Ali Biari <abiari@e1r2p2.1337.ma>
2020-12-28 14:27:33 +0100

    deleted influx setup.sh cuz it's useless

--- srcs/influxdb/srcs/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-influx -execute "create database telegraf"
-influx -execute "create user "telegraf" with password 'telegrafpass' with all privileges"
-influx -execute "grant all on "telegraf" to "telegraf""
